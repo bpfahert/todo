fddc249781489c8187cc35cfdeb8f4a4c47c4e29
Bryan <bpfahert@gmail.com>
2022-08-04 09:41:04 -0500

    Add ability to change and add projects
    Added ability to add new projects. clicking on a project will display its tasks.

--- src/dom.js
+++ src/dom.js
@@ -4,16 +4,18 @@ addTaskBtn.addEventListener('click', () => {
    document.getElementById("form").style.display="block";
 });
 
-function openForm() {
-    document.getElementById("form").style.display="block";
+//REWORK OPEN AND CLOSE FORMS TO SUPPORT PROJECT AND TASK FORMS
+function openForm(form) {
+    document.getElementById(form).style.display="block";
     
 }
 
-function closeForm() {
-    document.getElementById("form").style.display="none";
+function closeForm(form) {
+    document.getElementById(form).style.display="none";
 }
 
 function removeAllTasks() {
+    //REMOVE PROJECT TITLE
     let allTasks = document.querySelectorAll(".taskentry");
     allTasks.forEach(task => {
         task.remove();
@@ -22,7 +24,7 @@ function removeAllTasks() {
 
 
 function renderTasks(array) {
-    //Remove all task dom elements
+    //Remove all task dom elements, then render task array
     removeAllTasks();
     for (let i = 0; i < array.length; i++) {
     const taskList = document.querySelector(".tasklist")
@@ -39,6 +41,16 @@ function renderTasks(array) {
     let newPriority = document.createElement("li");
     newPriority.textContent = `Priority: ${array[i].priority}`;
 
+    let completeBtn = document.createElement("button");
+    completeBtn.setAttribute("type", "button");
+    completeBtn.classList += "completetaskbtn";
+    if (array[i].complete == true) {
+        completeBtn.textContent = "Completed";
+    }
+    else {
+    completeBtn.textContent = "Task Complete?";
+    };
+
     let removeBtn = document.createElement("button");
     removeBtn.setAttribute("type", "button");
     removeBtn.classList += "removetaskbtn";
@@ -51,6 +63,7 @@ function renderTasks(array) {
     newTask.appendChild(newDescription);
     newTask.appendChild(newDueDate);
     newTask.appendChild(newPriority);
+    newTask.appendChild(completeBtn);
     newTask.appendChild(removeBtn);
 
     removeBtn.addEventListener('click', () => {
@@ -62,15 +75,61 @@ function renderTasks(array) {
         }
         newTask.remove();
     });
+
+    //Only works once, won't switch back
+    completeBtn.addEventListener('click', () => {
+        if (completeBtn.textContent == "Task Complete?") {
+            array[i].complete = true;
+            completeBtn.textContent = "Completed";
+        }
+        else {
+            completeBtn.textContent == "Task Complete?";
+        }
+    });
+
     }
-   
+}
+
+//render the sidebar from projects array
+function renderSidebar(project) {
+    removeAllProjects();
+
+    //RemoveTitle() needed
+
+
+    for (let i = 0; i < project.length; i++) {
+        let addProjectBtn = document.querySelector(".addprojectbtn");
+        let projectList = document.querySelector(".projectlist");
+        let projectEntry = document.createElement("button");
+
+        projectEntry.setAttribute("type", "button");
+        projectEntry.classList += "project";
+        projectEntry.textContent = `${project[i].name}`;
+
+        projectList.insertBefore(projectEntry, addProjectBtn);
+
+        projectEntry.addEventListener('click', () => {
+            let currentProject = project[i].array;
+            let currentProjectTitle = document.querySelector(".currentprojecttitle");
+            currentProjectTitle.textContent = `${project[i].name}`;
+            renderTasks(currentProject);
+        });
+    };
+};
+
+function removeAllProjects() {
+    let allProjects = document.querySelectorAll(".project");
+    allProjects.forEach(project => {
+        project.remove();
+    });
 }
 
 
-
+ //check if needed to export removeAllTasks
 export {
     openForm,
     closeForm,
     renderTasks,
-    removeAllTasks
+    removeAllTasks,
+    renderSidebar
 };
--- src/index.js
+++ src/index.js
@@ -1,32 +1,77 @@
 import './style.css';
-import {removeAllTasks, closeForm, openForm, renderTasks} from './dom';
+import {removeAllTasks, closeForm, openForm, renderTasks, renderSidebar} from './dom';
 
-let projects = [];
-let defaultTasks = [];
-projects[0] = defaultTasks;
-
-const taskFactory = (title, description, dueDate, priority) => {
-
-    return {title, description, dueDate, priority};
+//Factory function to make projects
+const projectFactory = (projectName) => {
+    let name = projectName;
+    let array = [];
+    return {name, array};
 };
 
-const task = taskFactory('homework', 'do homework', 'tomorrow', 'important');
-const secondTask = taskFactory('dishes', 'do dishes', 'today', 'medium');
-defaultTasks.push(task);
-defaultTasks.push(secondTask);
-
-renderTasks(projects[0]);
+//initializing and test projects
+let projectArray = [];
+projectArray[0] = projectFactory("defaultTasks");
+projectArray[1] = projectFactory("tomorrow");
 
 
-let submitBtn = document.querySelector(".submitbtn");
+
+//Factory function to create task objects
+const taskFactory = (title, description, dueDate, priority, complete = false ) => {
+
+    return {title, description, dueDate, priority, complete};
+};
+
+
+//creates test tasks and puts them into test project arrays
+const task = taskFactory('homework', 'do homework', 'tomorrow', 'important', false);
+const secondTask = taskFactory('dishes', 'do dishes', 'today', 'medium', false);
+projectArray[0].array.push(task);
+projectArray[0].array.push(secondTask);
+const tomorrowTask = taskFactory('laundry');
+projectArray[1].array.push(tomorrowTask);
+
+//renders the defaultTasks array
+renderTasks(projectArray[0].array);
+
+//button for creating a new task 
+
+//TODO: ALLOW ADDING TASKS TO SELECTED PROJECT
+let submitBtn = document.querySelector(".submittaskbtn");
 submitBtn.addEventListener('click', () => {
     const newTitle = document.querySelector("#title");
     const newDescription = document.querySelector("#description");
     const newDueDate = document.querySelector("#duedate");
     const newPriority = document.querySelector("#priority");
     const newTask = taskFactory(`${newTitle.value}`, `${newDescription.value}`, `${newDueDate.value}`, `${newPriority.value}`);
-    defaultTasks.push(newTask);
-    renderTasks(projects[0]);
-    closeForm();
-    console.table(defaultTasks);
+    projectArray[0].array.push(newTask); 
+    renderTasks(projectArray[0].array);
+    closeForm("form");
 });
+
+//initial sidebar render
+renderSidebar(projectArray);
+
+//Button to add new projects
+let submitprojectBtn = document.querySelector(".projectsubmitbtn");
+submitprojectBtn.addEventListener('click', () => {
+    const newProjectName = document.querySelector("#projectname");
+    const newProject = projectFactory(`${newProjectName.value}`);
+    projectArray.push(newProject); 
+    renderSidebar(projectArray);
+    closeForm("projectform");
+
+});
+
+//button to open new project form
+let projectFormBtn = document.querySelector('.addprojectbtn');
+projectFormBtn.addEventListener('click', () => {
+    openForm("projectform");
+});
+
+
+// console.log(Object.keys({projects})[0]);
+console.log(projectArray[0].name);
+console.log(projectArray[0].array);
+console.table(projectArray);
+// console.log(projects[0].name);
+// console.log(defaultTasks.name);
